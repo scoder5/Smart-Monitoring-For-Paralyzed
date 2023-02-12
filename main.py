f002d4f1ac93a433e1f441b4227440599fb2d87d
scoder5 <scoding83@gmail.com>
2023-02-12 11:22:27 +0000

    new commit

--- main.py
+++ main.py
@@ -42,4 +42,4 @@ def user():
     return render_template("/MajorFE/examples/user.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="192.168.234.59")
+    app.run(debug=True, host="192.168.0.104")
