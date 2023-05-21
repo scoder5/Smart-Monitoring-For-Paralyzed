268dca86be4c028fb2604786f09090f97bab22de
scoder5 <scoding83@gmail.com>
2023-05-21 09:05:58 +0100

    Frontend Modified

--- main.py
+++ main.py
@@ -1,62 +1,62 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, redirect, session
 from dht import temp
 from testMAX30100 import pulse_oximeter
+import pyrebase
+import requests
 
 app = Flask(__name__)
 
-# config = {
-#     "apiKey": "AIzaSyB8kocn_vbBnFrWqnPhNgcLv4jf_0VSffw",
-#     "authDomain": "mjorproject.firebaseapp.com",
-#     "databaseURL": "https://mjorproject-default-rtdb.asia-southeast1.firebasedatabase.app",
-#     "projectId": "mjorproject",
-#     "storageBucket": "mjorproject.appspot.com",
-#     "messagingSenderId": "190239547334",
-#     "appId": "1:190239547334:web:2e5d0ea8d706278edbdcc7",
-#     "measurementId": "G-07CRCQB83Y"
-# }
+Config = {
+    "apiKey": "AIzaSyB8kocn_vbBnFrWqnPhNgcLv4jf_0VSffw",
+    "authDomain": "mjorproject.firebaseapp.com",
+    "databaseURL": "https://mjorproject-default-rtdb.asia-southeast1.firebasedatabase.app",
+    "projectId": "mjorproject",
+    "storageBucket": "mjorproject.appspot.com",
+    "messagingSenderId": "190239547334",
+    "appId": "1:190239547334:web:2e5d0ea8d706278edbdcc7",
+    "measurementId": "G-07CRCQB83Y"
+}
 
-# firebase = pyrebase.initialize_app(config)
-# db = firebase.database()
-# db.child("temperature").push({"value": 30})
-# db.child("temperature").update({"value": 30})
+firebase = pyrebase.initialize_app(Config)
+auth = firebase.auth()
+db = firebase.database()
+app.secret_key = "secret"
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/dashboard")
 def dashboard():
     res = temp()
-    return render_template("/MajorFE/examples/dashboard.html",  temperature=res, oxygen=pulse_oximeter())
+    return render_template("/examples/dashboard.html",  temperature=res, oxygen=pulse_oximeter())
+
 
 @app.route("/notifications")
 def notifications():
-    return render_template("/MajorFE/examples/notifications.html")
+    return render_template("/examples/notifications.html")
+
 
 @app.route("/icons")
 def icons():
-    return render_template("/MajorFE/examples/icons.html")
+    return render_template("/examples/icons.html")
 
-# @app.route("/user")
-# def user():
-#     return render_template("/MajorFE/examples/user.html")
 
 @app.route("/custom")
 def custom():
-    return render_template("/MajorFE/examples/custom.html")
+    return render_template("/examples/custom.html")
 
-@app.route("/admin")
-def admin():
-    res = temp()
-    return render_template("/MajorFE/examples/admin.html",  temperature=res, oxygen=pulse_oximeter())
 
-@app.route("/Signup")
-def Signup():
-    return render_template("/MajorFE/examples/Signup.html")
+@app.route("/signup")
+def signup():
+    return render_template("/examples/signup.html")
 
-@app.route("/Login")
-def Login():
-    return render_template("/MajorFE/examples/Login.html")
+
+@app.route("/login")
+def login():
+    return render_template("/examples/login.html")
+
 
 if __name__ == "__main__":
-    app.run(debug=True, host="192.168.0.103", port=8080)
+    app.run(debug=True, host="192.168.0.105", port=8080)
