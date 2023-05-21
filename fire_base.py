1b2715979ca8172182deb4b562838489599100df
scoder5 <scoding83@gmail.com>
2023-05-21 11:55:36 +0100

    Backend for Login/Signup is done

--- fire_base.py
+++ fire_base.py
@@ -1,19 +1,18 @@
 import pyrebase
 from dht import temp
 from time import sleep
+from config import Config
 
-config = {
-    "apiKey": "AIzaSyB8kocn_vbBnFrWqnPhNgcLv4jf_0VSffw",
-    "authDomain": "mjorproject.firebaseapp.com",
-    "databaseURL": "https://mjorproject-default-rtdb.asia-southeast1.firebasedatabase.app",
-    "projectId": "mjorproject",
-    "storageBucket": "mjorproject.appspot.com",
-    "messagingSenderId": "190239547334",
-    "appId": "1:190239547334:web:2e5d0ea8d706278edbdcc7",
-    "measurementId": "G-07CRCQB83Y"
-}
+apiKey = Config["apiKey"]
+authDomain = Config["authDomain"]
+databaseURL = Config["databaseURL"]
+projectId = Config["projectId"]
+storageBucket = Config["storageBucket"]
+messagingSenderId = Config["messagingSenderId"]
+appId = Config["appId"]
+measurementId = Config["measurementId"]
 
-firebase = pyrebase.initialize_app(config)
+firebase = pyrebase.initialize_app(Config)
 storage = firebase.storage()
 db = firebase.database()
 for i in range(5):
--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from flask import Flask, render_template, redirect, url_for, request, redirect, session
+from flask import Flask, render_template, redirect, request, redirect, session
 from dht import temp
 from testMAX30100 import pulse_oximeter
 import pyrebase
 import requests
+from config import Config
 
 app = Flask(__name__)
 
-Config = {
-    "apiKey": "AIzaSyB8kocn_vbBnFrWqnPhNgcLv4jf_0VSffw",
-    "authDomain": "mjorproject.firebaseapp.com",
-    "databaseURL": "https://mjorproject-default-rtdb.asia-southeast1.firebasedatabase.app",
-    "projectId": "mjorproject",
-    "storageBucket": "mjorproject.appspot.com",
-    "messagingSenderId": "190239547334",
-    "appId": "1:190239547334:web:2e5d0ea8d706278edbdcc7",
-    "measurementId": "G-07CRCQB83Y"
-}
+apiKey = Config["apiKey"]
+authDomain = Config["authDomain"]
+databaseURL = Config["databaseURL"]
+projectId = Config["projectId"]
+storageBucket = Config["storageBucket"]
+messagingSenderId = Config["messagingSenderId"]
+appId = Config["appId"]
+measurementId = Config["measurementId"]
+
 
 firebase = pyrebase.initialize_app(Config)
 auth = firebase.auth()
@@ -43,15 +43,67 @@ def custom():
     return render_template("/examples/custom.html")
 
 
-@app.route("/signup")
+@app.route("/signup", methods = ["GET", "POST"])
 def signup():
+
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+
+        if len(password) < 6:
+            return "Password should be at least 6 characters!"
+        
+        try:
+            user = auth.create_user_with_email_and_password(email, password)
+            if user:
+                session['user'] = email
+                return redirect('/dashboard')
+            else:
+                return "User creation failed"
+            
+        except requests.exceptions.HTTPError as e:
+            error_json = e.args[1]
+            error_message = error_json['error']['message']
+            if error_message == "EMAIL_EXISTS":
+                return "Email already exists. Please use a different email to sign up."
+            else:
+                return str(e)
+
+
     return render_template("/examples/signup.html")
 
 
-@app.route("/login")
+
+@app.route("/login", methods = ["GET", "POST"])
 def login():
+
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            session['user'] = email
+            return redirect('/dashboard')
+        
+        except requests.exceptions.HTTPError as e:
+            error_json = e.args[1]
+            error_message = error_json['error']['message']
+            if error_message == "EMAIL_NOT_FOUND":
+                return "Email not found. Please check your email and try again."
+            elif error_message == "INVALID_PASSWORD":
+                return "Invalid password. Please check your password and try again."
+            else:
+                return str(e)
+
     return render_template("/examples/login.html")
 
+
+
+@app.route("/logout")
+def logout():
+    session.pop('user', None)
+    return redirect('/')
 
 
 if __name__ == "__main__":
